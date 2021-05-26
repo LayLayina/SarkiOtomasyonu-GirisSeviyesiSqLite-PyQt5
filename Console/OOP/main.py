32eb3c21aff79b6e8fb35b37abd3214e74cb0aaa
LayLayina <77507576+LayLayina@users.noreply.github.com>
2021-05-26 23:21:18 +0300

    Update main.py

--- Console/OOP/main.py
+++ Console/OOP/main.py
@@ -1,18 +1,3 @@
-"""
-Proje 2
-Siz de bir tane Şarkı projesi geliştirmeye çalışın.
-Örnek özellikler;
-1. Şarkı İsmi 
-2. Sanatçı
- 3. Albüm
-4. Prodüksiyon Şirketi
-5. Şarkı Süresi
-Örnek Metodlar;
-1. Veritabanındaki Toplam Şarkı Süresini Hesaplayan Metod
-2. Şarkı Ekle
-3. Şarkı Sil
-"""
-
 import sqlite3 as sq
 
 class Sarkı():
